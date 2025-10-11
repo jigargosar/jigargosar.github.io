c7df2114e04dab511e2d39a55deb8d167876751f
Jigar Gosar <jigar.gosar@gmail.com>
2025-10-11 11:31:32 +0530

    feat: add Tailwind CSS integration and update configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 import rehypeAstroRelativeMarkdownLinks  from 'astro-rehype-relative-markdown-links'
 
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
-    markdown: {
-        rehypePlugins: [
-            rehypeAstroRelativeMarkdownLinks
-        ]
-    }
+  markdown: {
+      rehypePlugins: [
+          rehypeAstroRelativeMarkdownLinks
+      ]
+  },
+
+  vite: {
+    plugins: [tailwindcss()]
+  }
 });
