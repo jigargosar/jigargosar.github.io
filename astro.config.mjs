d1ea650470bcab241dae8c9df5471f8163a71647
Jigar Gosar <jigar.gosar@gmail.com>
2025-10-11 10:32:12 +0530

    build: add GitHub Actions workflow for deploying to GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,11 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import rehypeRelativeLinks from 'astro-rehype-relative-markdown-links'
+import rehypeAstroRelativeMarkdownLinks  from 'astro-rehype-relative-markdown-links'
 
-const base = process.env.BASE_PATH || '/';
 export default defineConfig({
-    base,
-    vite: {
-        server: {
-            watch: {
-                ignored: ['**/docs/**']
-            }
-        }
-    },
     markdown: {
         rehypePlugins: [
-            [rehypeRelativeLinks, { base }]
+            rehypeAstroRelativeMarkdownLinks
         ]
     }
 });
