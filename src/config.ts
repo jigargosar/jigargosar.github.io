94a066226c5b7f53b50955e098b495756f69bf4c
Jigar Gosar <jigar.gosar@gmail.com>
2025-10-22 15:18:30 +0530

    style: refactor BaseLayout and related components to use SITE and AUTHOR constants for improved maintainability

---
+++ src/config.ts
@@ -0,0 +1,10 @@
+export const SITE = {
+  title: 'Jigar Gosar - The Functional Programmer',
+  description: 'Functional programming insights and TIL moments from a developer\'s journey',
+  url: 'https://jigargosar.github.io/',
+}
+
+export const AUTHOR = {
+  name: 'Jigar Gosar',
+  url: 'https://jigargosar.github.io/',
+}
