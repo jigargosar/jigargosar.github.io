6b545c2384d1ef50f4fe18ef606ad499eec18f7d
Jigar Gosar <jigar.gosar@gmail.com>
2025-10-22 15:58:04 +0530

    style: add config file for SITE and AUTHOR constants to enhance clarity and maintainability

---
+++ src/config.ts
@@ -0,0 +1,8 @@
+export const SITE = {
+  title: 'Jigar Gosar - The Functional Programmer',
+  description: 'Functional programming insights and TIL moments from a developer\'s journey',
+}
+
+export const AUTHOR = {
+  name: 'Jigar Gosar',
+}
