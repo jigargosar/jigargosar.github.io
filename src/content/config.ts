3d7ca41c51828aa9cf8b88b85b298b674d931df5
Jigar Gosar <jigar.gosar@gmail.com>
2025-11-05 12:30:21 +0530

    fix: standardize formatting in post schema definition

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,14 +1,14 @@
 import { defineCollection, z } from 'astro:content'
-import { image } from 'astro:assets'
 
 const posts = defineCollection({
-  schema: ({ image }) => z.object({
-    title: z.string(),
-    date: z.string().date(),
-    intro: z.string(),
-    image: image().optional(),
-    imageAlt: z.string().optional(),
-  }),
+  schema: ({ image }) =>
+    z.object({
+      title: z.string(),
+      date: z.string().date(),
+      intro: z.string(),
+      image: image().optional(),
+      imageAlt: z.string().optional(),
+    }),
 })
 
 export const collections = {
